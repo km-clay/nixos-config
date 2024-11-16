a6e86076896c9ed25afc8008f8c2eaaed12ad329
pagedmov <kylerclay@proton.me>
2024-11-15 20:21:20 -0500

    updated flake inputs, deleted panda3ds overlay pkg

--- overlay/overlay.nix
+++ overlay/overlay.nix
@@ -4,6 +4,5 @@ self: super: {
     tinyfetch = super.callPackage ./tinyfetch/package.nix {};
     breezex-cursor = super.callPackage ./breezex-cursor/package.nix {};
     check_updates = super.callPackage ./check_updates/package.nix {};
-    panda3ds = super.callPackage ./panda3ds/package.nix {};
   };
 }
